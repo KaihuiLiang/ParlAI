f5ae552db598eaa0b502b6195fb0f213ac385ff8
Kai-Hui Liang <cafe.liang@gmail.com>
2022-02-07 03:12:59 -0500

    Fix conflict

--- parlai/agents/local_human/local_human.py
+++ parlai/agents/local_human/local_human.py
@@ -87,22 +87,12 @@ class LocalHumanAgent(Agent):
             raise StopIteration
 
         ##### Add start
-<<<<<<< HEAD
-        # if reply_text == " ":
-        #     reply['text'] = "[START]"
-        # #####
-        # else:
-        #     reply['text'] = reply_text
-
-        reply['text'] = reply_text
-=======
         if reply_text == " ":
             reply['text'] = "[START]"
         #####
         else:
             reply['text'] = reply_text
 
->>>>>>> 8b82365fabc2db034e21196320bcedb443da46a6
 
         if '[EXIT]' in reply_text:
             self.finished = True
